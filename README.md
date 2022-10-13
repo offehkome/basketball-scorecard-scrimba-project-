379463e6f64774bae110a4f89479adeb91ea527c
Oferah <75563382+offehkome@users.noreply.github.com>
2022-10-13 19:37:44 +0100

    Add files via upload

---
+++ README.md
@@ -0,0 +1,4 @@
+# BASKETBALL SCORECARD PROJECT(scrimba project)
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/oferah21/pen/qBRGezL](https://codepen.io/oferah21/pen/qBRGezL).
+
---
+++ script.js
@@ -0,0 +1,23 @@
+let homeScore = document.getElementById("home-score");
+let guestScore = document.getElementById("guest-score");
+let result = 0;
+let count = 0;
+
+function addOne(){
+  homeScore.textContent = result++;
+}
+function addTwo(){
+  homeScore.textContent = result += 2;
+}
+function addThree(){
+  homeScore.textContent = result += 3;
+}
+function addOneMore(){
+  guestScore.innerText = count++;
+}
+function addTwoMore(){
+  guestScore.textContent = count += 2;
+}
+function addThreeMore(){
+  guestScore.textContent = count += 3;
+}
